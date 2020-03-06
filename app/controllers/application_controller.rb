4ca537cd4e3a0f40050b89b658a53bb9ae39441b
Alex Baulderstone <alex.baulderstone@gmail.com>
2020-03-07 10:53:48 +1100

    added fields to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
 helper_method :resource_name, :resource, :devise_mapping, :resource_class
+protect_from_forgery with: :exception
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+
 def resource_name
   :user
 end
@@ -14,4 +21,12 @@ end
 def devise_mapping
   @devise_mapping ||= Devise.mappings[:user]
 end
+
+protected
+
+     def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :location, :email, :password)}
+
+          devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :location, :email, :password, :current_password)}
+     end
 end
