56eb51cbe826c5f790bd7714b6e956d2d56947c8
Alex Baulderstone <alex.baulderstone@gmail.com>
2020-03-13 16:47:57 +1100

    added helper for favorites

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,15 @@
 module GamesHelper
+    def favorite_link(game)
+        if current_user
+             if current_user.favorite_ids.include?(game.id)
+                return link_to icon('fas', 'star'), favorite_game_path(game, type: "unfavorite"), method: :put
+            else
+                return link_to icon('far', 'star'), favorite_game_path(game, type: "favorite"), method: :put
+            end
+            return link_to icon('far', 'star'), new_user_registration_path
+                
+
+        end 
+
+    end 
 end
